2b52f30cab8b4f1573bce58b8b3e4db864cb2425
IlyaSolovyov <solovyov909@gmail.com>
2018-04-01 23:21:22 +0300

    Initial commit.

---
+++ NewsfeedAPIService/Controllers/PostsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NewsfeedAPIService.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    public class PostsController : Controller
+    {
+
+        [HttpGet("digests/{digestId}")]
+        public IActionResult GetPostsByDigest(int digestId)
+        {
+            return Ok("Succesfully asked for posts by id#" + digestId);
+        }
+    }
+}
